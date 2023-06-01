3edb58f5d624a9527940e1b53bb5860bf0b0c6e3
SomberTM <sombertm@gmail.com>
2023-06-01 16:28:52 -0400

    Finish CRUD operations for organizations

--- src/api/controllers/organizations.rs
+++ src/api/controllers/organizations.rs
@@ -1,70 +1,74 @@
 use crate::models::{NewOrganization, Organization};
 use crate::DbPool;
 
-use actix_web::{get, post, web, HttpResponse, Responder};
+use actix_web::{delete, get, post, put, web, HttpResponse, Responder};
 use chrono::Utc;
 use diesel::prelude::*;
 
 // TODO: ERRORS NEED TO PROPOGATE THROUGH web::block CALLS FOR PROPER HttpResponse STATUS CODE.
 
-#[get("/organizations")]
+#[get("")]
 pub async fn get_organizations(pool: web::Data<DbPool>) -> impl Responder {
     let get_organizations_result = web::block(move || {
-        let mut connection = pool
+        let connection = &mut pool
             .get()
             .expect("Couldn't get database connection from pool");
 
-        use crate::schema::organizations::dsl::*;
+        use crate::schema::organizations::dsl::organizations;
 
         organizations
-            .load::<Organization>(&mut connection)
+            .load::<Organization>(connection)
             .expect("Error loading organizations")
     })
     .await;
 
-    if let Ok(orgs) = get_organizations_result {
-        HttpResponse::Ok().json(orgs)
-    } else {
-        HttpResponse::InternalServerError().finish()
+    match get_organizations_result {
+        Ok(organizations) => HttpResponse::Ok().json(organizations),
+        Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
-#[get("/organizations/{organization_id}")]
+#[get("/{organization_id}")]
 pub async fn get_organization(
     pool: web::Data<DbPool>,
     path: web::Path<(String,)>,
 ) -> impl Responder {
     let (organization_id,): (String,) = path.into_inner();
+    let organization_uuid = uuid::Uuid::parse_str(&organization_id);
+
+    if let Err(_) = organization_uuid {
+        return HttpResponse::BadRequest().finish();
+    }
+
+    let organization_uuid = organization_uuid.unwrap();
+
     let get_organization_result = web::block(move || {
-        let mut connection = pool
+        let connection = &mut pool
             .get()
             .expect("Couldn't get database connection from pool");
 
-        use crate::schema::organizations::dsl::*;
+        use crate::schema::organizations::dsl::{id, organizations};
 
-        let organization_uuid =
-            uuid::Uuid::parse_str(&organization_id).expect("Invalid UUID provided");
         organizations
             .filter(id.eq(organization_uuid))
-            .first::<Organization>(&mut connection)
+            .first::<Organization>(connection)
             .expect("Couldn't find organization") as Organization
     })
     .await;
 
-    if let Ok(organization) = get_organization_result {
-        HttpResponse::Ok().json(organization)
-    } else {
-        HttpResponse::InternalServerError().finish()
+    match get_organization_result {
+        Ok(organization) => HttpResponse::Ok().json(organization),
+        Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
-#[post("/organizations")]
+#[post("")]
 pub async fn create_organization(
     pool: web::Data<DbPool>,
     json: web::Json<NewOrganization>,
 ) -> impl Responder {
     let create_organization_result = web::block(move || {
-        let mut connection = pool
+        let connection = &mut pool
             .get()
             .expect("Couldn't get database connection from pool");
 
@@ -76,14 +80,83 @@ pub async fn create_organization(
 
         diesel::insert_into(crate::schema::organizations::table)
             .values(&new_organization)
-            .get_result::<Organization>(&mut connection)
+            .get_result::<Organization>(connection)
             .expect("Error creating organization")
     })
     .await;
 
-    if let Ok(new_organization) = create_organization_result {
-        HttpResponse::Ok().json(new_organization)
-    } else {
-        HttpResponse::InternalServerError().finish()
+    match create_organization_result {
+        Ok(new_organization) => HttpResponse::Ok().json(new_organization),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+#[put("/{organization_id}")]
+pub async fn update_organization(
+    pool: web::Data<DbPool>,
+    json: web::Json<NewOrganization>,
+    path: web::Path<(String,)>,
+) -> impl Responder {
+    let (organization_id,): (String,) = path.into_inner();
+    let organization_uuid = uuid::Uuid::parse_str(&organization_id);
+
+    if let Err(_) = organization_uuid {
+        return HttpResponse::BadRequest().finish();
+    }
+
+    let organization_uuid = organization_uuid.unwrap();
+
+    let new_name = json.into_inner().name;
+
+    let update_organization_result = web::block(move || {
+        let connection = &mut pool
+            .get()
+            .expect("Couldn't get database connection from pool");
+
+        use crate::schema::organizations::dsl::{name, organizations};
+
+        diesel::update(organizations.find(organization_uuid))
+            .set(name.eq(new_name))
+            .get_result::<Organization>(connection)
+            .expect("Could not update organization")
+    })
+    .await;
+
+    match update_organization_result {
+        Ok(organization) => HttpResponse::Ok().json(organization),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+#[delete("/{organization_id}")]
+pub async fn delete_organization(
+    pool: web::Data<DbPool>,
+    path: web::Path<(String,)>,
+) -> impl Responder {
+    let (organization_id,): (String,) = path.into_inner();
+    let organization_uuid = uuid::Uuid::parse_str(&organization_id);
+
+    if let Err(_) = organization_uuid {
+        return HttpResponse::BadRequest().finish();
+    }
+
+    let organization_uuid = organization_uuid.unwrap();
+
+    let delete_organization_result = web::block(move || {
+        let connection = &mut pool
+            .get()
+            .expect("Couldn't get database connection from pool");
+
+        use crate::schema::organizations::dsl::{id, organizations};
+
+        diesel::delete(organizations.filter(id.eq(organization_uuid)))
+            .get_result::<Organization>(connection)
+            .expect("Error deleting organization")
+    })
+    .await;
+
+    match delete_organization_result {
+        Ok(organization) => HttpResponse::Ok().json(organization),
+        Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,28 +11,25 @@ pub type DbPool = r2d2::Pool<r2d2::ConnectionManager<PgConnection>>;
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
-    dbg!("Starting main");
-
     dotenv().ok();
-    dbg!("Loaded .env variables");
-
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    dbg!("Loaded DATABASE_URL");
 
     let manager = r2d2::ConnectionManager::<PgConnection>::new(database_url);
-    dbg!("Setup connection manager");
-
     let pool = r2d2::Pool::builder()
         .build(manager)
         .expect("DATABASE_URL should be a valid PostgreSQL connection string");
-    dbg!("Built connnection pool {:?}", &pool);
+
+    use api::controllers;
 
     HttpServer::new(move || {
-        App::new()
-            .app_data(web::Data::new(pool.clone()))
-            .service(api::controllers::organizations::get_organization)
-            .service(api::controllers::organizations::get_organizations)
-            .service(api::controllers::organizations::create_organization)
+        App::new().app_data(web::Data::new(pool.clone())).service(
+            web::scope("/organizations")
+                .service(controllers::organizations::get_organization)
+                .service(controllers::organizations::get_organizations)
+                .service(controllers::organizations::create_organization)
+                .service(controllers::organizations::update_organization)
+                .service(controllers::organizations::delete_organization),
+        )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
