1be0120ba82aee41dcfddc7f6c3ca490b1366be7
SomberTM <sombertm@gmail.com>
2023-06-02 00:05:01 -0400

    Fix .env.example and used env var LOCAL_FILESTORE_DIR

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ pub type DbPool = r2d2::Pool<r2d2::ConnectionManager<PgConnection>>;
 async fn main() -> io::Result<()> {
     dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let filestore_dir = env::var("LOCAL_FILESTORE_DIR").expect("LOCAL_FILESTORE_DIR must be set");
 
     let manager = r2d2::ConnectionManager::<PgConnection>::new(database_url);
     let pool = r2d2::Pool::builder()
@@ -49,7 +50,9 @@ async fn main() -> io::Result<()> {
                             .service(controllers::organizations::upload_organization_files),
                     ),
             )
-            .service(Files::new("/fileserver", "./filestore/").prefer_utf8(true))
+            .service(
+                Files::new("/fileserver", format!("{}/", filestore_dir.clone())).prefer_utf8(true),
+            )
     })
     .bind(("127.0.0.1", 8080))?
     .run()
